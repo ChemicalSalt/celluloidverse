85273c4a0cf82e5d98396e7275490f3ed81d961f
ChemicalSalt <chemistry50551555@gmail.com>
2025-08-20 16:07:39 +0530

    Fix root route to work without trailing slash

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Client, GatewayIntentBits } = require("discord.js");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (_req, res) => res.send("Bot is alive"));
+app.get("*", (_req, res) => res.send("Bot is alive"));
 app.listen(PORT, () => console.log(`Web server on ${PORT}`));
 
 const client = new Client({
