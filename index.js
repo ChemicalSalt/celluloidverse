1fab90026dc5333404b308fc984ad03e5c55fc4a
ChemicalSalt <chemistry50551555@gmail.com>
2025-08-20 15:06:38 +0530

    Initial command

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (_req, res) => res.send("Bot is alive"));
+app.listen(PORT, () => console.log(`Web server on ${PORT}`));
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN); 
