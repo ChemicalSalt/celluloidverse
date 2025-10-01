6d70d95cac2fcf61dd86462300e06fb8b135ffd1
ChemicalSalt <chemistry50551555@gmail.com>
2025-10-01 14:51:38 +0530

    '

--- bot/plugins/farewell.js
+++ bot/plugins/farewell.js
@@ -1,4 +1,19 @@
-const { handleFarewell } = require("./welcome"); // same as welcome logic
+function formatMessage(msg, member) {
+  if (!msg) return "";
+  return msg
+    .replaceAll("{username}", member.user.username)
+    .replaceAll("{usermention}", `<@${member.id}>`)
+    .replaceAll("{server}", member.guild.name)
+    .replace(/\{role:([^\}]+)\}/g, (_, r) => {
+      const role = member.guild.roles.cache.find(x => x.name === r);
+      return role ? `<@&${role.id}>` : r;
+    })
+    .replace(/\{channel:([^\}]+)\}/g, (_, c) => {
+      const ch = member.guild.channels.cache.find(x => x.name === c);
+      return ch ? `<#${ch.id}>` : c;
+    });
+}
+
 async function handleFarewell(member, plugin) {
   if (!plugin?.enabled) return;
 
@@ -6,12 +21,12 @@ async function handleFarewell(member, plugin) {
     const ch = member.guild.channels.cache.get(plugin.channelId) ||
       await member.guild.channels.fetch(plugin.channelId).catch(() => null);
     if (ch?.permissionsFor(member.guild.members.me)?.has("SendMessages")) {
-      await ch.send(plugin.serverMessage.replaceAll("{username}", member.user.username)).catch(() => {});
+      await ch.send(formatMessage(plugin.serverMessage, member)).catch(() => {});
     }
   }
 
   if (plugin.sendInDM && plugin.dmMessage) {
-    await member.send(plugin.dmMessage.replaceAll("{username}", member.user.username)).catch(() => {});
+    await member.send(formatMessage(plugin.dmMessage, member)).catch(() => {});
   }
 }
 
